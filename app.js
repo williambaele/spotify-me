42c10cb5366c15e2094fc87110867c519c440ba8
William Baele <baelewilliam1999@gmail.com>
2023-03-17 12:37:16 +0100

    Added JS error handling

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 function getUserData() {
   const username = document.getElementById("username").value;
   const url = `https://api.spotify.com/v1/users/${username}`;
-  const accessToken = "BQAyeKQmBuD4Og-Ia6utzC-V4I8sj69V7idlSzEEgGbDkIXSrieQBJ-Vs4m7CK2i-TjAzsD9AvAI_orzX5Ym6agnEWiE7_CZ2JNuQ-FRTXDscvPIyhXASK6J9JrwbfoPkPkiQo7mIjL5s5p3ly75EwBPGhU9q5YGD5kDWNg8QQmLvJhTWCE1DwOA"; // Replace with your own access token
+  const accessToken = "BQCSOxa_oirAauvxMQqUJvVuopP6uhYsk3f39Mp0Iugr8CYEk-7ZexBsv8vrKltSdjxApMK1UvzcSWZbybBCAFmDXSK8opTdJJNkj9HCsLS9UocteaG1fodKP_5Dm5X5Cry-N5EUbKe9SGKOJ2PnoRqFTLGboyX5aZb5Tz6nQlW6f8rYvKEhT9cAvtUdcR4Ny-Lz";
 
   fetch(url, {
     headers: {
       'Authorization': 'Bearer ' + accessToken
     }
   })
-  .then(response => response.json())
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else if (response.status === 404) {
+      throw new Error("User not found");
+    } else {
+      throw new Error("Request failed");
+    }
+  })
   .then(data => {
-    // Do something with the user data, such as display it on the page
+    // Do something with the user data
     console.log(data);
-    console.log("User well loaded")
     let name = data.display_name;
     document.querySelector('#username2').textContent = name;
     console.log(name);
   })
-  .catch(error => console.error(error));
+  .catch(error => {
+    console.log('Error:', error);
+    if (error.message === "User not found") {
+      document.querySelector('#searchbtn').textContent = "Error";
+      document.querySelector('#searchbtn').classList.add('bg-[#FF0000]');
+    }
+  });
 }
